33c8b0c0139c26ce14819164a4f2cd6adf75c9a3
Scott DeVito <scottadevito@gmail.com>
2018-06-03 22:29:45 -0400

    Create AppBarNav

--- src/components/reusable/app_bar_nav.MUI.js
+++ src/components/reusable/app_bar_nav.MUI.js
@@ -12,16 +12,37 @@ const styles = {
   flex: {
     flex: 1,
   },
+  bar: {
+    background:
+      'linear-gradient(113.33deg, #c4c4c4 0%, #058ed9 0.01%,#0069c0 100%)',
+    boxShadow: '0 0 0 0',
+  },
+  navButton: {
+    background: 'rgba(110, 198, 255, 0.38)',
+    marginLeft: '1rem',
+    marginRight: '1rem',
+    fontWeight: 600,
+    borderRadius: '4px',
+  },
 };
 
 function AppBarNav(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar className={classes.bar} position="static">
         <Toolbar>
           <div variant="title" color="inherit" className={classes.flex} />
-          <Button color="inherit">Nav Links</Button>
+          <Button color="inherit" className={classes.navButton}>
+            Home
+          </Button>
+          <Button color="inherit" className={classes.navButton}>
+            Coaching Staff
+          </Button>
+          <Button color="inherit" className={classes.navButton}>
+            Clinics
+          </Button>
+          631-494-5040
         </Toolbar>
       </AppBar>
     </div>
